3a9dcc4649c31a524be21846494d3088ab31c510
Md. Sohag Hossain <sohaghossain7th@gmail.com>
2025-03-27 14:47:01 +0600

    update

--- src/utils/baseUrl.js
+++ src/utils/baseUrl.js
@@ -1,9 +1,9 @@
 export const getBaseUrl = () => {
   // return "http://10.0.70.35:8025/api/v1";
-  return "https://api.yourjewel.nl/api/v1";
+  return "https://api.your-jewel.com/api/v1";
 };
 
 export const getImageUrl = () => {
   // return "http://10.0.70.35:8025/api/v1";
-  return "https://api.yourjewel.nl";
+  return "https://api.your-jewel.com";
 };
